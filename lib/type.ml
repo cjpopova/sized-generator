21e6f31f685fc6e2e5c4c78a81f8df8017a9cf15
cjpopova <casparpopova2023@u.northwestern.edu>
2025-06-26 11:42:09 -0400

    Check list.length before for_all2

--- lib/type.ml
+++ lib/type.ml
@@ -12,7 +12,8 @@ let rec is_same_ty tyl1 tyl2 =
   | (FlatTyBool, FlatTyBool) -> true
   | (FlatTyInt, FlatTyInt) -> true
   | (FlatTyArrow (doms1, cod1), FlatTyArrow (doms2, cod2)) -> 
-    List.for_all2 is_same_ty doms1 doms2
+    List.length doms1 = List.length doms2
+    && List.for_all2 is_same_ty doms1 doms2
     && is_same_ty cod1 cod2
   | (_, _) -> false
 
